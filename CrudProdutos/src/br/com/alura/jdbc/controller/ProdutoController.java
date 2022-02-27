6581b8c1264c2108d148cd78b709f9dd508bc062
sen1t <gabrielsiqueiradev@gmail.com>
2022-02-27 10:40:52 -0300

    Enviando o projeto

---
+++ CrudProdutos/src/br/com/alura/jdbc/controller/ProdutoController.java
@@ -0,0 +1,37 @@
+package br.com.alura.jdbc.controller;
+
+import java.sql.Connection;
+import java.util.List;
+
+import br.com.alura.jdbc.dao.ProdutoDAO;
+import br.com.alura.jdbc.factory.ConnectionFactory;
+import br.com.alura.jdbc.modelo.Produto;
+
+public class ProdutoController {
+	
+	private ProdutoDAO produtoDAO;
+	
+	public ProdutoController() {
+		   Connection connection = 
+				   new ConnectionFactory().recuperarConexao();
+		   this.produtoDAO = new ProdutoDAO(connection);
+	}
+
+	public void deletar(Integer id) {
+		produtoDAO.deletar(id);
+		System.out.println("Deletando produto");
+	}
+
+	public void salvar(Produto produto) {
+		produtoDAO.salvarComCategoria(produto);
+		System.out.println("Salvando produto");
+	}
+
+	public List<Produto> listar() {
+		return produtoDAO.listar();
+	}
+
+	public void alterar(String nome, String descricao, Integer id) {
+		produtoDAO.alterar(nome, descricao, id);
+	}
+}
